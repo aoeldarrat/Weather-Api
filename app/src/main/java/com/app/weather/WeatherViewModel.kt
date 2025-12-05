fcb254a4f46605f96d19225da98e0778ee1dfa9b
aoeldarrat <abudyd@gmail.com>
2025-12-05 16:16:41 -0600

    Update WeatherViewModel to support new api data

--- app/src/main/java/com/app/weather/WeatherViewModel.kt
+++ app/src/main/java/com/app/weather/WeatherViewModel.kt
@@ -4,6 +4,7 @@ import WeatherService
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.app.weather.model.PointDataResponse
 import com.app.weather.model.WeatherResponse
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -20,8 +21,12 @@ class WeatherViewModel(
     private val weatherService: WeatherService
 ) : ViewModel() {
 
-    private val _weatherData = MutableStateFlow<WeatherResponse?>(null)
-    val weatherData: StateFlow<WeatherResponse?> = _weatherData.asStateFlow()
+    // Deprecated api logic
+//    private val _weatherData = MutableStateFlow<WeatherResponse?>(null)
+//    val weatherData: StateFlow<WeatherResponse?> = _weatherData.asStateFlow()
+
+    private val _pointData = MutableStateFlow<PointDataResponse?>(null)
+    val pointData: StateFlow<PointDataResponse?> = _pointData.asStateFlow()
 
     private val _isLoading = MutableStateFlow(false)
     val isLoading: StateFlow<Boolean> = _isLoading.asStateFlow()
@@ -29,50 +34,83 @@ class WeatherViewModel(
     private val _error = MutableStateFlow<String?>(null)
     val error: StateFlow<String?> = _error.asStateFlow()
 
-    private val _searchQuery = MutableStateFlow("")
-    val searchQuery: StateFlow<String> = _searchQuery.asStateFlow()
+    private val _latitudeSearchQuery = MutableStateFlow("")
+    val latitudeSearchQuery: StateFlow<String> = _latitudeSearchQuery.asStateFlow()
 
-    private val _currentCity = MutableStateFlow("London")
-    val currentCity: StateFlow<String> = _currentCity.asStateFlow()
+    private val _longitudeSearchQuery = MutableStateFlow("")
+    val longitudeSearchQuery: StateFlow<String> = _longitudeSearchQuery.asStateFlow()
+
+    private val _currentLatitude = MutableStateFlow(0.0)
+    private val _currentLongitude = MutableStateFlow(0.0)
+    val currentLatitude: StateFlow<Double> = _currentLatitude.asStateFlow()
+    val currentLongitude: StateFlow<Double> = _currentLongitude.asStateFlow()
 
     init {
-        _searchQuery
+        _latitudeSearchQuery
             .debounce(500L)
             .filter { query ->
                 query.isNotBlank()
             }.onEach {
-                getWeatherData(it.trim())
+                if (!it.isEmpty() && !longitudeSearchQuery.value.isEmpty()) {
+                    getWeatherData(
+                        it.trim().toDouble(),
+                        longitudeSearchQuery.value.trim().toDouble()
+                    )
+                }
+            }.launchIn(viewModelScope)
+
+        _longitudeSearchQuery
+            .debounce(500L)
+            .filter { query ->
+                query.isNotBlank()
+            }.onEach {
+                if (!it.isEmpty() && !latitudeSearchQuery.value.isEmpty()) {
+                    getWeatherData(
+                        latitudeSearchQuery.value.trim().toDouble(),
+                        it.trim().toDouble()
+                    )
+                }
             }.launchIn(viewModelScope)
     }
 
-    fun getWeatherData(city: String) {
+    fun getWeatherData(latitude: Double, longitude: Double) {
         viewModelScope.launch {
             try {
                 _isLoading.value = true
                 _error.value = null
-                _currentCity.value = city
+                _currentLatitude.value = latitude
+                _currentLongitude.value = longitude
+                // Deprecated api response code
+//                val response = weatherService.getWeather(city)
+//                _weatherData.value = response
 
-                val response = weatherService.getWeather(city)
-                _weatherData.value = response
+                val response = weatherService.getPointData(latitude, longitude)
+                _pointData.value = response
 
-                Log.d("WeatherViewModel", "Weather data loaded successfully for $city")
+                Log.d("WeatherViewModel", "Weather data loaded successfully for lat: $latitude, long: $longitude" )
             } catch (e: Exception) {
                 val errorMessage = "Failed to load weather data: ${e.message}"
                 _error.value = errorMessage
-                _weatherData.value = null
+                _pointData.value = null
 
-                Log.e("WeatherViewModel", "Error fetching weather data for $city", e)
+                Log.e("WeatherViewModel", "Error fetching weather data for lat: $latitude, long: $longitude", e)
             } finally {
                 _isLoading.value = false
             }
         }
     }
 
-    fun updateSearchQuery(query: String) {
-        _searchQuery.value = query
+    fun updateSearchQuery(lat: String?, long: String?) {
+        lat?.let {
+            _latitudeSearchQuery.value = lat
+        }
+
+        long?.let {
+            _longitudeSearchQuery.value = long
+        }
     }
 
     fun retryCurrentSearch() {
-        getWeatherData(_currentCity.value)
+        getWeatherData(currentLatitude.value, currentLongitude.value)
     }
 }
