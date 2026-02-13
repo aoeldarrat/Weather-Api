16c86d6bb8e8d50e49d078bd59b18e424359405b
aoeldarrat <abudyd@gmail.com>
2026-02-13 15:26:25 -0600

    change constants structure

--- app/src/main/java/com/app/weather/utilities/Constants.kt
+++ app/src/main/java/com/app/weather/utilities/Constants.kt
@@ -1,5 +1,18 @@
 package com.app.weather.utilities
 
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
 object Constants {
-    val numOfOffScreenPage: Int = 4
+    const val numOfOffScreenPage: Int = 4
+}
+
+object Dp {
+    val quarterStandard = 2.dp
+    val halfStandard = 4.dp
+    val standard = 8.dp
+    val standAndHalf = 12.dp
+    val doubleStandard = 16.dp
+    val tripleStandard = 24.dp
+    val quadrupleStandard = 32.dp
 }
