c36d574e12d4280800d228b3b296b82385a77400
aoeldarrat <abudyd@gmail.com>
2026-02-13 15:26:07 -0600

    route comments

--- app/src/main/java/com/app/weather/navigation/Routes.kt
+++ app/src/main/java/com/app/weather/navigation/Routes.kt
@@ -1,13 +1,19 @@
 package com.app.weather.navigation
 
 import kotlinx.serialization.Serializable
+// Landing page
+@Serializable
+object Home
 
+// Weather page (deprecated?)
 @Serializable
 data class Weather(val state: String)
 
+// Settings page
 @Serializable
 object Settings
 
+// Interactive map
 @Serializable
 object MapPage
 
